738fcf2d5833a2e4da312b493177dbd036a6377b
Yousef Salah <yousefsalah1242003@gmail.com>
2025-02-13 21:44:21 +0200

    so;ve static file issue

--- backend/index.js
+++ backend/index.js
@@ -28,12 +28,10 @@ app.use("/api/auth",authRouter)
 app.use("/api/message",messageRouter)
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "frontend", "dist");
-
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
